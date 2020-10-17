8197e001b91d165134ab6894a63439a47716fb42
Cyber-43 <72963771+Cyber-43@users.noreply.github.com>
2020-10-17 17:45:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class product:
 root = Tk()
 root.title("E-INVOICE GENERATOR")
 width = 750
-height = 600  
+height = 290  
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = (screen_width / 2) - (width / 2)
